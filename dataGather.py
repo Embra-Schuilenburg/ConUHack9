a098b58a4a85175f89701d9fefc842cc13348781
em-is-aligned <t7ash@protonmail.com>
2025-02-02 14:30:42 -0500

    POST JUDGING-Implemented single variable overall data

--- dataGather.py
+++ dataGather.py
@@ -7,13 +7,7 @@ import os
 import csv
 
 os.chdir("C:\software\Pycharm\conUHacks9")
-with open('Data/statsCleaned.csv', newline='\n') as csvfile:
-    # data is read from the relative CSV containing match data, first row of the list contains column headers.
-    # The 0th element of the row-0 is empty to respect that the 0th column stores the deck archetypes players can
-    # select from. NOTE: There are more user selection options (120) than meta options (32)
-    data = list(csv.reader(csvfile))
 
-os.chdir('../Credentials/')
 # fix later
 credential_file = open("C:\software\Pycharm\Credentials\EngiQDB.txt")
 
@@ -28,36 +22,15 @@ try:
 except Exception as e:
     print(e)
 
-
-
-# Old, populates Overall, DO NOT UNCOMMENT
-# mydb = client["MTGVersus"]
-# mycol = mydb["Overall"]
-#
-# i = 1
-# while i < len(data):
-#     post = {"player": data[i][0], "confidence": data[i][1], "WR": data[i + 1][1], "sampleSize": data[i + 2][1]}
-#     x = mycol.insert_one(post)
-#     i = i + 3
-
-# Old, populates Matchups, DO NOT UNCOMMENT
-# mydb = client["MTGVersus"]
-# mycol = mydb["Matchups"]
-# i = 1
-# j = 2
-# while j <= len(data[1]):
-#     while i < len(data):
-#         post = {"player": data[i][0], "opp": data[0][j], "confidence": data[i][1], "WR": data[i + 1][1], "sampleSize": data[i + 2][1]}
-#         x = mycol.insert_one(post)
-#         i = i + 3
-#     j = j + 1
-
 mydb = client["MTGVersus"]
-mycol = mydb["Overall"]
+OverallCollection = mydb["Overall"]
 
-mydoc = mycol.find().sort("player", -1)
+def getSingleArchetypeOverallData(archetype):
+    archetypeQuery = {"player": archetype}
+    data = OverallCollection.find(archetypeQuery, {"_id": 0, "archetype": 0 })
+    for data in data:
+        return(data.get("confidence"), data.get("WR"), data.get("sampleSize"))
 
-for x in mydoc:
-  print(x)
+print(getSingleArchetypeOverallData("Izzet Phoenix"))
 
 credential_file.close()
