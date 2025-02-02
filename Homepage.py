4f72130b464d9242171599b99e2e178f2d077287
Epoch <vincivin2608@gmail.com>
2025-02-01 23:02:23 -0500

    Multipage navigation works

--- Homepage.py
+++ Homepage.py
@@ -1,18 +1,30 @@
 import streamlit as st
+#from streamlit_option_menu import option_menu
 
 
 # ---------- Setup ------------ #
-st.set_page_config(
-    page_title="MTGVersus",
-    page_icon="meow",
-    )
+
+# Setup Page Navigation
+lib = st.Page("pages/1_library.py", title="Grimoire",)
+meta = st.Page("pages/2_meta.py", title="Strategy Oracle",)
+tier = st.Page("pages/3_tier_list.py", title="Tier List",)
+spin = st.Page("pages/4_spin.py", title="Summoning Ritual",)
+
+# Common Attributes
+st.sidebar.title("Most Credible Source 100 emoji")
+st.sidebar.markdown(":blue[*The Magic that gathered as statistics rules the land.... only one may rise against the onslaught of the **Trumpinator***]")
+st.logo(
+    image="Data/circle.png",
+    icon_image="Data/circle.png",
+    size="large"
+)
+
+# Init Multipage
+pg = st.navigation([lib, meta, tier, spin])
+pg.run()
 
 
-st.title("MTGVersus")
 
-#side bar
-st.sidebar.success("Select page")
-#image("Data/circle.jpg", caption="MTGVersus")
 
 
 
