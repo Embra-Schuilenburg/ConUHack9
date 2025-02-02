7a1c6aaf8e495d150382961c29423f82d0971a70
em-is-aligned <t7ash@protonmail.com>
2025-02-02 09:22:43 -0500

    FINALLY GOT THE OVERALL IN THE DB

--- dataGather.py
+++ dataGather.py
@@ -1,21 +1,41 @@
-archetypes = "Izzet Phoenix, Azorius Control, Rakdos Demons, Selesnya Company, Enigmatic Incarnation, Mono Black Demons, Lotus Field Combo, Green Devotion, Mardu Greasefang, Rakdos Midrange, Abzan Greasefang, Domain Zur, Azorius Spirits, 5 Color Niv-Mizzet, Azorius Lotus Field, Boros Tokens Control, Jund Sacrifice, Azorius Humans, Mono White Humans, Rakdos Transmogrify, Rakdos Prowess, Orzhov Demons, Quintorius Combo, Boros Burn, Dimir Ninjas, Esper Control, 5 Color Landfall, Mono Black, Red Deck Wins, Izzet Creativity, Atarka Red, Golgari Midrange"
-izPh = ""
-archetypes = archetypes.replace(" \t", ", ")
-#print(archetypes)
+import pathlib
 
+from pymongo import MongoClient
+import certifi
+from pymongo import MongoClient, collection
+import os
 import csv
 
+os.chdir("C:\software\Pycharm\conUHacks9")
 with open('Data/statsCleaned.csv', newline='\n') as csvfile:
     # data is read from the relative CSV containing match data, first row of the list contains column headers.
     # The 0th element of the row-0 is empty to respect that the 0th column stores the deck archetypes players can
     # select from. NOTE: There are more user selection options (120) than meta options (32)
     data = list(csv.reader(csvfile))
 
-output = []
-for row in data:
-    if row[0] not in output:
-        output.append(row[0])
-print(output)
+os.chdir('../Credentials/')
+# fix later
+credential_file = open("C:\software\Pycharm\Credentials\EngiQDB.txt")
 
-for archetypes in output:
-    print(archetypes, end=",")
+MONGO_URI = str(credential_file.readline())  # From your personal credential file
+
+# Connect to MongoDB
+client = MongoClient(MONGO_URI)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Access granted")
+except Exception as e:
+    print(e)
+
+mydb = client["MTGVersus"]
+mycol = mydb["Overall"]
+
+i = 1
+while i < len(data) - 417:
+    post = {"player": data[i][0], "confidence": data[i][1], "WR": data[i + 1][1], "sampleSize": data[i + 2][1]}
+    x = mycol.insert_one(post)
+    i = i + 3
+
+
+credential_file.close()
