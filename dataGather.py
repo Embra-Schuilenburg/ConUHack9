843399928fb5e2e0be203bef1b71aa0314e11a1b
em-is-aligned <t7ash@protonmail.com>
2025-02-06 12:52:59 -0500

    Fixing data pt1

--- dataGather.py
+++ dataGather.py
@@ -22,3 +22,15 @@ def getSingleArchetypeOverallData(archetype):
     data = OverallCollection.find(archetypeQuery, {"_id": 0, "archetype": 0 })
     for data in data:
         return(data.get("WR"), data.get("sampleSize"), data.get("confidence"))
+
+# Old, populates Matchups, DO NOT UNCOMMENT
+# mydb = client["MTGVersus"]
+# mycol = mydb["Matchups"]
+# i = 1
+# j = 2
+# while j <= len(data[1]):
+#     while i < len(data):
+#         post = {"player": data[i][0], "opp": data[0][j], "confidence": data[i][1], "WR": data[i + 1][1], "sampleSize": data[i + 2][1]}
+#         x = mycol.insert_one(post)
+#         i = i + 3
+#     j = j + 1
